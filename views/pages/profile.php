7a517f0145ab43f2569639871c3e2dd9b914b9b6
GGermanBoldyrev <boldyrev_german@mail.ru>
2023-08-21 00:23:51 +0300

    profile page added

--- views/pages/profile.php
+++ views/pages/profile.php
@@ -1,9 +1,38 @@
 <main class="flex justify-center items-center flex-col h-[calc(100vh-85px)]">
-    <div class="bg-blue-400 flex w-[900px] py-[10px] px-[20px] rounded">
-        <img src="images/user.png" alt="user" class="w-[100px]">
-        <div class="text-black">
-            <div>Email: boldyrev_german@mail.ru</div>
-            <div>Password: ******</div>
+    <div class="bg-blue-400 flex justify-center py-[20px] px-[50px] rounded">
+        <div class="flex">
+            <div class="flex flex-col items-center">
+                <div class="rounded-full">
+                    <img src="images/user.png" alt="user" class="w-[100px]">
+                </div>
+                <div class="text-lg mt-[20px]">
+                    <a href="#"
+                       class="border border-black px-[6px] rounded hover:text-white hover:border-white duration-200">
+                        Change profile image
+                    </a>
+                </div>
+            </div>
+            <div class="mx-[50px] border border-black"></div>
+            <div class="text-black flex flex-col justify-between">
+                <div>
+                    <!--Email-->
+                    <div>Email: boldyrev_german@mail.ru</div>
+                    <!--Пароль-->
+                    <div class="flex mt-[5px]">
+                        <div>Password: ******</div>
+                        <div class="text-lg ml-[30px]">
+                            <a href="#" class="underline hover:text-white duration-200">Change password</a>
+                        </div>
+                    </div>
+                </div>
+                <!--Кнопка log out-->
+                <div class="text-lg">
+                    <a href="#" class="border border-black px-[6px] rounded hover:text-white hover:border-white
+                                       duration-200">
+                        Log out
+                    </a>
+                </div>
+            </div>
         </div>
     </div>
 </main>
