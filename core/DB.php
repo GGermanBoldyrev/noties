bedbe10730f844b62904cd552394fba54bc9bce1
GGermanBoldyrev <boldyrev_german@mail.ru>
2023-08-28 17:15:49 +0300

    db class changes, __construct now take parameters to connect

--- core/DB.php
+++ core/DB.php
@@ -9,14 +9,11 @@ class DB
     // PDO instance
     private PDO $db;
 
-    // TODO: брать connect из .env файла
-    public function __construct()
+    // Create a new PDO instance
+    public function __construct(string $host, string $dbname, string $username, string $password, string $charset = 'utf-8')
     {
-        // Database configuration parameters
-        $dbconfig = require_once '../db.config.php';
-
         // New PDO connection settings
-        $dsn = 'mysql:host=' . $dbconfig['HOST'] . ';dbname=' . $dbconfig['DBNAME'] . ';charset=' . $dbconfig['CHARSET'];
+        $dsn = 'mysql:host=' . $host . ';dbname=' . $dbname . ';charset=' . $charset;
         $opt = [
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
@@ -24,14 +21,6 @@ class DB
         ];
 
         // Create PDO instance
-        $this->db = new PDO($dsn, $dbconfig['USERNAME'], $dbconfig['PASSWORD'], $opt);
-    }
-
-    // Returns all results from query as an array[]
-    public function getAll($queryName): array
-    {
-        $sql = "SELECT * FROM $queryName";
-        $query = $this->pdo->query($sql);
-        return $query->fetchAll();
+        $this->db = new PDO($dsn, $username, $password, $opt);
     }
 }
